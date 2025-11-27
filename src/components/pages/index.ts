7de4bfe9941e2443ddbf0d9af499dae5b2495354
pinpetfun <thisgoothisgood@gmail.com>
2025-11-27 15:28:21 +0800

    加空投10万sol

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,3 +2,4 @@ export { default as HomePage } from './HomePage.jsx';
 export { default as CreatePage } from './CreatePage.jsx';
 export { default as TradeCenterPage } from './TradeCenterPage.jsx';
 export { default as DebugPage } from './DebugPage.jsx';
+export { ResourcesPage } from './ResourcesPage';
